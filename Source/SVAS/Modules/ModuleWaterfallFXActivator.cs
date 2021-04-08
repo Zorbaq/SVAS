a8a4a33273e4b5c6eb23679f3f41820ced8f871c
zorbaq <19189627+Zorbaq@users.noreply.github.com>
2021-04-08 12:56:39 +0200

    Merged into SVAS

---
+++ Source/SVAS/Modules/ModuleWaterfallFXActivator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Waterfall;
+
+namespace SVAS.Modules
+{
+    public class ModuleWaterfallFXActivator : ModuleWaterfallFXToggler
+    {
+        /// <summary>
+        /// In theory, the name of the module that, when active, sets the defined Waterfall controller to 1.0
+        /// </summary>
+        [KSPField]
+        public string activatorModule = string.Empty;
+
+        //private bool controllerIsEnabled = false;
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            Events["ToggleWaterfallController"].active = false;
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            var module = part.Modules.GetModule(activatorModule);
+
+            // Apparently, each module type needs its own handling
+
+            if (module is ModuleResourceHarvester moduleResourceHarvester)
+            {
+                // The problem with ResourceStatus is that it becomes "n/a" on scene load even on active converters
+                //if (moduleResourceHarvester.ResourceStatus != "n/a")
+
+                if (moduleResourceHarvester.status != null) // Status becomes null when ore is full and PAW is closed :D
+                {
+                    if (moduleResourceHarvester.status == "Operational" || moduleResourceHarvester.status.Contains("% load"))
+                    {
+                        EnableWaterfallController();
+                    }
+                    else
+                    {
+                        DisableWaterfallController();
+                    }
+                }
+            }
+            else if (module is ModuleAsteroidDrill || module is ModuleCometDrill)
+            {
+                BaseDrill potatoDrill = module as BaseDrill;
+
+                if (potatoDrill.status != null)
+                {
+                    if (potatoDrill.status == "Connected")
+                    {
+                        EnableWaterfallController();
+                    }
+                    else
+                    {
+                        DisableWaterfallController();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/SVAS/Modules/ModuleWaterfallFXToggler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Waterfall;
+
+namespace SVAS
+{
+    public class ModuleWaterfallFXToggler : PartModule
+    {
+        [KSPField]
+        public string controllerName = string.Empty;
+
+        [KSPEvent(guiActive = true, guiName = "Toggle Waterfall Controller")]
+        public void ToggleWaterfallController()
+        {
+            ModuleWaterfallFX waterfallModule = GetWaterfallModule();
+
+            if (waterfallModule != null)
+            {                
+                float controllerOverrideValue = waterfallModule.GetControllerValue(controllerName)[0]; // No more than 1 controller per part please!
+
+                waterfallModule.SetControllerOverride(controllerName, true);
+                waterfallModule.SetControllerOverrideValue(controllerName, Math.Abs(controllerOverrideValue -= 1f));
+
+                //UnityEngine.Debug.Log("Toggling waterfall module!");
+            }
+        }
+
+        public bool EnableWaterfallController()
+        {
+            ModuleWaterfallFX waterfallModule = GetWaterfallModule();
+
+            waterfallModule.SetControllerOverride(controllerName, true);
+            waterfallModule.SetControllerOverrideValue(controllerName, 1f);
+
+            return true;
+        }
+        public bool DisableWaterfallController()
+        {
+            ModuleWaterfallFX waterfallModule = GetWaterfallModule();
+
+            waterfallModule.SetControllerOverride(controllerName, true);
+            waterfallModule.SetControllerOverrideValue(controllerName, 0f);
+
+            return false;
+        }
+        private ModuleWaterfallFX GetWaterfallModule()
+        {
+            var partModuleList = this.part.Modules;
+
+            //UnityEngine.Debug.Log("This part: " + part.name);
+            //UnityEngine.Debug.Log("PartModuleList count: " + partModuleList.Count);
+
+            foreach (PartModule module in this.part.Modules)
+            {
+                //UnityEngine.Debug.Log(module.moduleName);
+                if (module.moduleName == "ModuleWaterfallFX")
+                {
+                    //UnityEngine.Debug.Log("Found " + module.moduleName + "!");
+                    return module as ModuleWaterfallFX;
+                }
+            }
+            //UnityEngine.Debug.Log("No waterfall module found!");
+            return null;
+        }
+    }
+}
